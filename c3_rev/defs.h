ffdb2326f53222d9d8ca0e9016bd5d1b3d2e4a70
dalerank <dalerankn8@gmail.com>
2014-06-03 20:44:31 +0400

    good old games

--- c3_rev/defs.h
+++ c3_rev/defs.h
@@ -15,7 +15,125 @@ struct C3GraphicOffsetID
   int graphicId;
 };
 
-typedef unsigned int BuildingType;
+
+typedef int BuildingType;
+
+enum BuildingTypeT
+{
+  B_Menu_Farms     = 2,
+  B_Menu_RawMaterials  = 3,
+ B_Menu_Workshops  = 4,
+ B_Road           = 5,
+ B_Wall           = 6,
+ B_ReservoirSimon  = 7,
+ B_Aqueduct       = 8,
+ B_ClearLand      = 9,
+ B_HouseVacantLot  = 10,
+ B_HouseLargeTent  = 11,
+ B_HouseSmallShack  = 12,
+ B_HouseLargeShack  = 13,
+ B_HouseSmallHovel  = 14,
+ B_HouseLargeHovel  = 15,
+ B_HouseSmallCasa  = 16,
+ B_HouseLargeCasa  = 17,
+ B_HouseSmallInsula  = 18,
+ B_HouseMediumInsula  = 19,
+ B_HouseLargeInsula  = 20,
+ B_HouseGrandInsula  = 21,
+ B_HouseSmallVilla  = 22,
+ B_HouseMediumVilla  = 23,
+ B_HouseLargeVilla  = 24,
+ B_HouseGrandVilla  = 25,
+ B_HouseSmallPalace  = 26,
+ B_HouseMediumPalace  = 27,
+ B_HouseLargePalace  = 28,
+ B_HouseLuxuryPalace  = 29,
+ B_Amphitheater   = 30,
+ B_Theater        = 31,
+ B_Hippodrome     = 32,
+ B_Colosseum      = 33,
+ B_GladiatorSchool  = 34,
+ B_LionHouse      = 35,
+ B_ActorColony    = 36,
+ B_ChariotMaker   = 37,
+ B_Plaza          = 38,
+ B_Gardens        = 39,
+ B_FortLegionaries  = 40,
+ B_SmallStatue    = 41,
+ B_MediumStatue   = 42,
+ B_LargeStatue    = 43,
+ B_FortJavelin    = 44,
+ B_FortMounted    = 45,
+ B_Doctor         = 46,
+ B_Hospital       = 47,
+ B_Bathhouse      = 48,
+ B_Barber         = 49,
+ B_DistributionCenter_Unused  = 50,
+ B_School         = 51,
+ B_Academy        = 52,
+ B_Library        = 53,
+ B_FortGround     = 54,
+ B_Prefecture     = 55,
+ B_TriumphalArch  = 56,
+ B_FortGround__   = 57,
+ B_Gatehouse      = 58,
+ B_Tower          = 59,
+ B_SmallTempleCeres  = 60,
+ B_SmallTempleNeptune  = 61,
+ B_SmallTempleMercury  = 62,
+ B_SmallTempleMars  = 63,
+ B_SmallTempleVenus  = 64,
+ B_LargeTempleCeres  = 65,
+ B_LargeTempleNeptune  = 66,
+ B_LargeTempleMercury  = 67,
+ B_LargeTempleMars  = 68,
+ B_LargeTempleVenus  = 69,
+ B_Market         = 70,
+ B_Granary        = 71,
+ B_Warehouse      = 72,
+ B_WarehouseSpace  = 73,
+ B_Shipyard       = 74,
+ B_Dock           = 75,
+ B_Wharf          = 76,
+ B_GovernorsHouse  = 77,
+ B_GovernorsVilla  = 78,
+ B_GovernorsPalace  = 79,
+ B_MissionPost    = 80,
+ B_EngineersPost  = 81,
+ B_LowBridge      = 82,
+ B_ShipBridge     = 83,
+ B_Senate         = 84,
+ B_SenateUpgraded  = 85,
+ B_Forum          = 86,
+ B_ForumUpgraded  = 87,
+ B_NativeHut      = 88,
+ B_NativeMeeting  = 89,
+ B_Reservoir      = 90,
+ B_Fountain       = 91,
+ B_Well           = 92,
+ B_NativeCrops    = 93,
+ B_MilitaryAcademy  = 94,
+ B_Barracks       = 95,
+ B_Menu_SmallTemples  = 96,
+ B_Menu_LargeTemples  = 97,
+ B_Oracle         = 98,
+ B_BurningRuin    = 99,
+ B_WheatFarm      = 100,
+ B_VegetableFarm  = 101,
+ B_FruitFarm      = 102,
+ B_OliveFarm      = 103,
+ B_VinesFarm      = 104,
+ B_PigFarm        = 105,
+ B_MarbleQuarry   = 106,
+ B_IronMine       = 107,
+ B_TimberYard     = 108,
+ B_ClayPit        = 109,
+ B_WineWorkshop   = 110,
+ B_OilWorkshop    = 111,
+ B_WeaponsWorkshop  = 112,
+ B_FurnitureWorkshop  = 113,
+ B_PotteryWorkshop  = 114
+};
 
 struct C3CustomButton
 {
@@ -23,8 +141,8 @@ struct C3CustomButton
   unsigned short yStart;
   unsigned short xEnd;
   unsigned short yEnd;
-  unsigned int mouseClickHandler;
-  unsigned int rightClickHangler;
+  void (*mouseClickHandler)(C3CustomButton*, unsigned int);
+  void (*rightClickHandler)(int, unsigned char);
   unsigned char field_10;
   unsigned char field_11;
   unsigned char field_12;
@@ -33,11 +151,26 @@ struct C3CustomButton
   unsigned int parameter2;
 };
 
+struct C3ArrowButton
+{
+  unsigned short xOffset;
+  unsigned short yOffset;
+  unsigned short graphicId;
+  unsigned short size;
+  void (*mouseClickHandler)();
+  unsigned char field_C;
+  unsigned char field_D;
+  unsigned char field_E;
+  unsigned char field_F;
+  unsigned int parameter;
+  unsigned int parameter2;
+};
+
 struct C3MenuItem
 {
   unsigned short yOffset;
   unsigned short textNumber;
-  int handler;
+  void (*handler)();
   int field_8;
   int parameter;
 };
@@ -93,8 +226,8 @@ struct C3ImageButton
   unsigned short graphicCollectionId;
   unsigned short graphicIdOffsetInCollection;
   unsigned short field_E;
-  unsigned int mouseClickHandler;
-  unsigned int rightClickHangler;
+  void (*mouseClickHandler)();
+  unsigned int rightClickHandler;
   unsigned char state_enabled;
   unsigned char field_19;
   unsigned char field_1A;
@@ -123,83 +256,7 @@ const int Emp_DistantBattleRomanArmy = 7;
 const int Emp_DistantBattleEnemyArmy = 8;
 
 
-const BuildingType B_HouseVacantLot = 0xA;
-const BuildingType B_ClearLand = 0x9;
-const BuildingType B_Road = 0x5;
-const BuildingType B_ReservoirSimon = 0x7;
-const BuildingType B_Aqueduct = 0x8;
-const BuildingType B_Fountain = 0x5B;
-const BuildingType B_Well = 0x5c;
-const BuildingType B_Barber = 0x31;
-const BuildingType B_Bathhouse = 0x30;
-const BuildingType B_Doctor = 0x2e;
-const BuildingType B_Hospital = 0x2f;
-const BuildingType B_Menu_SmallTemples = 0x60;
-const BuildingType B_Menu_LargeTemples = 0x61;
-const BuildingType B_Oracle = 0x62;
-const BuildingType B_School = 0x33;
-const BuildingType B_Academy = 0x34;
-const BuildingType B_Library = 0x35;
-const BuildingType B_Theater = 0x1f;
-const BuildingType B_Amphitheater = 0x1e;
-const BuildingType B_Colosseum = 0x21;
-const BuildingType B_Hippodrome = 0x20;
-const BuildingType B_GladiatorSchool = 0x22;
-const BuildingType B_LionHouse = 0x23;
-const BuildingType B_ActorColony = 0x24;
-const BuildingType B_ChariotMaker = 0x25;
-const BuildingType B_Forum = 0x56;
-const BuildingType B_SenateUpgraded = 0x55;
-const BuildingType B_GovernorsHouse = 0x4d;
-const BuildingType B_GovernorsVilla = 0x4e;
-const BuildingType B_GovernorsPalace = 0x4f;
-const BuildingType B_SmallStatue = 0x29;
-const BuildingType B_MediumStatue = 0x2a;
-const BuildingType B_LargeStatue = 0x2b;
-const BuildingType B_Gardens = 0x27;
-const BuildingType B_Plaza = 0x26;
-const BuildingType B_EngineersPost = 0x51;
-const BuildingType B_MissionPost = 0x50;
-const BuildingType B_Shipyard  = 0x4a;
-const BuildingType B_Dock = 0x4b;
-const BuildingType B_Wharf = 0x4c;
-const BuildingType B_Wall = 0x6;
-const BuildingType B_Tower = 0x3b;
-const BuildingType B_Gatehouse = 0x3a;
-const BuildingType B_Prefecture  = 0x37;
-const BuildingType B_FortGround__= 0x39;
-const BuildingType B_MilitaryAcademy = 0x5e;
-const BuildingType B_Barracks  = 0x5f;
-const BuildingType B_DistributionCenter_Unused = 0x32;
-const BuildingType B_Menu_Farms  = 0x2;
-const BuildingType B_Menu_RawMaterials  = 0x3;
-const BuildingType B_Menu_Workshops  = 0x4;
-const BuildingType B_Market = 0x46;
-const BuildingType B_Granary = 0x47;
-const BuildingType B_Warehouse = 0x48;
-const BuildingType B_LowBridge = 0x52;
-const BuildingType B_Reservoir = 0x5a;
-const BuildingType B_ShipBridge = 0x53;
-const BuildingType B_TriumphalArch = 0x38;
-const BuildingType B_HouseLuxuryPalace = 0x1D;
-const BuildingType B_WheatFarm = 0x64;
-const BuildingType B_VegetableFarm = 0x65;
-const BuildingType B_FruitFarm = 0x66;
-const BuildingType B_OliveFarm = 0x67;
-const BuildingType B_VinesFarm = 0x68;
-const BuildingType B_PigFarm = 0x69;
-const BuildingType B_ClayPit = 0x6d;
-const BuildingType B_MarbleQuarry = 0x6a;
-const BuildingType B_IronMine = 0x6b;
-const BuildingType B_TimberYard = 0x6c;
-const BuildingType B_WineWorkshop  = 0x6e;
-const BuildingType B_OilWorkshop = 0x6f;
-const BuildingType B_WeaponsWorkshop = 0x70;
-const BuildingType B_FurnitureWorkshop = 0x71;
-const BuildingType B_PotteryWorkshop = 0x72;
-const BuildingType B_NativeHut = 0x58;
-const BuildingType B_NativeMeeting = 0x59;
-const BuildingType B_NativeCrops = 0x5D;
+
 typedef unsigned int GoodType;
 
 const GoodType G_Wheat = 1;
@@ -265,7 +322,26 @@ enum OverlayType
   Overlay_Damage=9,
   Overlay_Crime=0xA,
   Overlay_Problems=0x1D,
-  Overlay_Water=0x2
+  Overlay_Desirability=0x1A,
+  Overlay_Entertainment=11,
+  Overlay_Education=16,
+  Overlay_Theater=12,
+  Overlay_FoodStocks=25,
+  Overlay_Religion=4,
+  Overlay_Native =0x1C,
+  Overlay_Water=0x2,
+  Overlay_Barber=20,
+  Overlay_School=17,
+  Overlay_Library=18,
+  Overlay_Bathhouse=21,
+  Overlay_TaxIncome=24,
+  Overlay_Amphiheater=13,
+  Overlay_Academy  = 19,
+  Overlay_Hospital  = 23,
+  Overlay_Colosseum=14,
+  Overlay_Hippodrome=15,
+  Overlay_Clinic=22,
+  Overlay_Workers  = 27
 };
 
 enum AdvisorType {
@@ -285,7 +361,10 @@ enum AdvisorType {
 
 enum FontType
 {
+  F_NormalBlack=0x86,
+  F_LargeBlack=0x29e,
   F_SmallBrown=0x430
+
 };
 
 enum TradeStatusType {
@@ -293,6 +372,100 @@ enum TradeStatusType {
   Trade_Import=1,
   Trade_Export=2
 };
+
+enum TerrainType {
+  T_Tree           = 1,
+ T_Rock           = 2,
+ T_Water          = 4,
+ T_Building       = 8,
+ T_Scrub          = 0x10,
+ T_Garden         = 0x20,
+ T_Road           = 0x40,
+ T_ReservoirRange  = 0x80,
+ T_Aqueduct       = 0x100,
+ T_Elevation      = 0x200,
+ T_AccessRamp     = 0x400,
+ T_Meadow         = 0x800,
+ T_FountainRange  = 0x2000,
+ T_Wall           = 0x4000,
+ T_Gatehouse      = 0x8000
+
+};
+
+enum WalkerType
+{
+Walker_Immigrant  = 1,
+Walker_Emigrant  = 2,
+Walker_Homeless  = 3,
+Walker_CartPusher  = 4,
+Walker_LaborSeeker  = 5,
+Walker_Explosion  = 6,
+Walker_TaxCollector  = 7,
+Walker_Engineer  = 8,
+Walker_Warehouseman  = 9,
+Walker_Prefect   = 10,
+Walker_FortJavelin  = 11,
+Walker_FortMounted  = 12,
+Walker_FortLegionary  = 13,
+Walker_FortStandard  = 14,
+Walker_Actor     = 15,
+Walker_Gladiator  = 16,
+Walker_LionTamer  = 17,
+Walker_Charioteer  = 18,
+Walker_TradeCaravan  = 19,
+Walker_TradeShip  = 20,
+Walker_TradeCaravanDonkey  = 21,
+Walker_Protestor  = 22,
+Walker_Criminal  = 23,
+Walker_Rioter    = 24,
+Walker_FishingBoat  = 25,
+Walker_MarketTrader  = 26,
+Walker_Priest    = 27,
+Walker_SchoolChild  = 28,
+Walker_Teacher   = 29,
+Walker_Librarian  = 30,
+Walker_Barber    = 31,
+Walker_BathhouseWorker  = 32,
+Walker_Doctor    = 33,
+Walker_Surgeon   = 34,
+Walker_MapFlag   = 36,
+Walker_Flotsam   = 37,
+Walker_Dockman   = 38,
+Walker_MarketBuyer  = 39,
+Walker_Patrician  = 40,
+Walker_IndigenousNative  = 41,
+Walker_TowerSentry  = 42,
+Walker_Enemy43   = 43,
+Walker_Enemy44   = 44,
+Walker_Enemy45   = 45,
+Walker_Enemy46   = 46,
+Walker_Enemy47   = 47,
+Walker_Enemy48   = 48,
+Walker_Enemy49   = 49,
+Walker_Enemy50   = 50,
+Walker_Enemy51   = 51,
+Walker_Enemy52   = 52,
+Walker_Enemy53   = 53,
+Walker_Enemy54   = 54,
+Walker_Enemy55   = 55,
+Walker_Enemy56   = 56,
+Walker_Enemy57   = 57,
+Walker_NativeTrader  = 58,
+Walker_Arrow     = 59,
+Walker_Javelin   = 60,
+Walker_Bolt      = 61,
+Walker_Ballista  = 62,
+Walker_Creature  = 63,
+Walker_Missionary  = 64,
+Walker_FishGulls  = 65,
+Walker_DeliveryBoy  = 66,
+Walker_Shipwreck  = 67,
+Walker_Sheep     = 68,
+Walker_Wolf      = 69,
+Walker_Zebra     = 70,
+Walker_Spear     = 71,
+Walker_HippodromeMiniHorses  = 72
+}
 
 
 #endif
